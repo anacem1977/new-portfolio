47cf1741e500cf54373d4486db68530f58eb8c9a
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-29 17:15:32 -0600

    intro page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import react, { Component } from "react"
+import animate__animated from "animate.css"
+import TextLoop from "react-text-loop"
 
 class App extends Component {
   constructor(props) {
@@ -9,12 +11,20 @@ class App extends Component {
 
     } 
   }
-  render() {
+
+  render( ) {
     return (
       <div className="App">
-
+        <section className="animate__animated animate__fadeInDown animate__delay-3s">
+            <p>Menu</p>
+        </section>
+        <div>
+          <TextLoop interval={[3000]}>
+              <h1><span className="animate__animate animate__bounce">HELLO</span></h1>
+              <h1><span>HOLA.</span></h1>
+            </TextLoop>
+        </div>
       </div>
-
     )
   }
 } 
