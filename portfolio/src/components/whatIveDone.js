e7997799c89524c2a26a1323ad6614791f309453
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-30 09:50:38 -0600

    added links to GA and HR

--- portfolio/src/components/whatIveDone.js
+++ portfolio/src/components/whatIveDone.js
@@ -9,6 +9,7 @@ render () {
             <div>
                 <h3>What I've done?</h3>
                 <p>I'm still learning, but here are the links to the projects I did while attending the Software Engineering Reskilling program by General Assembly.</p>
+                <p>After finishing the program, I've continued learning and practicing my skills, so you'll find as well the badges and certifications I've received on <a href="https://www.hackerrank.com/">Hacker Rank</a> so far.</p>
             </div>
 
         </div>
--- portfolio/src/components/whereIveBeen.js
+++ portfolio/src/components/whereIveBeen.js
@@ -10,7 +10,7 @@ render () {
                 <h3>Where I've been?</h3>
                 <p>I have a major in Industrial Engineering from Tecnológico de Monterrey and I've worked for John Deere for a little over nineteen years at the Engines (Motores) facility located in Torreón, Coahuila, México with twelve years of experience in Product Engineering and Specifications and about seven years in Supply Management for major diesel engine components.</p>
                 <p>During this time, I've also had the opportunity to participate in numerous projects such as SAP implementation, sunset of one of the engines that was produced at the factory, asset management and many more.</p>
-                <p>Part of being curious is that I love learning new stuff: I'm <em>almost</em> level B1 in French, I have a Diploma on Data Science and I've decided to take a big turn in my careeer so, I recently finished the Software Engineering Immersive Reskilling Program with General Assembly (they are awesome!)</p>
+                <p>Part of being curious is that I love learning new stuff: I'm <em>almost</em> level B1 in French, I have a Diploma on Data Science and I've decided to take a big turn in my careeer so, I recently finished the Software Engineering Immersive Reskilling Program with <a href="https://generalassemb.ly/">General Assembly</a> (they are awesome!)</p>
             </div>
 
         </div>
