0a9e64b4dafa24667a755ddfdc0165f0e7c91603
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-30 08:21:52 -0600

    hello message working, capturing scroll

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,29 +1,58 @@
-import React from 'react';
 import './App.css';
-import react, { Component } from "react"
+import react, {Component} from "react"
 import animate__animated from "animate.css"
-import TextLoop from "react-text-loop"
+import { Link, Route } from "react-router-dom"
+import WhoAmI from "./components/whoAmI"
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state= {
-
-    } 
+    this.state = {
+      change: true,
+    }
   }
+
+  handleEnglish = (event) => {
+    this.setState({
+      change:false
+    })
+  }
+
+  handleSpanish = (event) => {
+    this.setState({
+      change:true
+    })
+  }
+
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll, {passive: true})
+  }
+  componentWillUnmount(){
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+  
+  handleScroll(event) {
+    console.log("scrolling")
+    console.log(event.path[1].scrollY)
+  }
+  
 
   render( ) {
     return (
-      <div className="App">
-        <section className="animate__animated animate__fadeInDown animate__delay-3s">
+      <div className="App" handleScroll={this.hanldeScroll}>
+        <section className="animate__animated animate__fadeInDown animate__delay-1s">
             <p>Menu</p>
         </section>
         <div>
-          <TextLoop interval={[3000]}>
-              <h1><span className="animate__animate animate__bounce">HELLO</span></h1>
-              <h1><span>HOLA.</span></h1>
-            </TextLoop>
+            {this.state.change === true ? <h1 onMouseEnter={this.handleEnglish} className="animate__animated animate__swing">HELLO</h1> : <h1 onMouseEnter={this.handleSpanish} className="animate__animated animate__bounce">HOLA</h1> }
         </div>
+        <div>
+            <Link to ="/">scroll 👇🏻</Link>
+        </div>
+
+        <Route path="/" render= {(props) => ( <WhoAmI/>)} />
+
       </div>
     )
   }
---
+++ portfolio/src/components/whoAmI.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class WhoAmI extends Component {
+
+
+render () {
+    return (
+        <div className="whoAmI">
+            <div>
+                <h3>MY NAME IS</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie bibendum eros vel tempor. Praesent ac vestibulum tortor. Proin quis iaculis eros, eu pretium enim. Quisque ultricies lacus eu sapien faucibus, ut laoreet erat finibus. Maecenas sodales feugiat risus, ac dictum arcu placerat sit amet. Phasellus eu purus sit amet nisi lobortis malesuada. Nunc placerat elementum ante a hendrerit. Proin scelerisque in ipsum a ultrices.
+
+Fusce fringilla enim leo, id gravida arcu gravida nec. Aenean viverra urna eu nunc convallis, at pellentesque tellus facilisis. Nam venenatis iaculis sagittis. Vestibulum in enim in mauris sodales accumsan a sed tellus. Nulla facilisi. Fusce a orci ex. Suspendisse hendrerit diam at elit sagittis, eget luctus libero pellentesque. Nulla facilisi. Quisque vestibulum, lacus mollis imperdiet hendrerit, augue lectus efficitur ligula, ut fermentum mi arcu varius mauris.
+
+Praesent scelerisque varius diam, eu dignissim tortor eleifend eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut urna orci, molestie et metus vel, accumsan dictum mi. Curabitur tempor arcu elementum pulvinar imperdiet. Curabitur a pretium lorem. Duis porttitor finibus turpis, ut lobortis nunc ultricies vel. Nulla non vestibulum orci, ac bibendum nisl. Morbi quis congue metus, a ornare magna. Ut accumsan est arcu, eu pellentesque odio placerat at. Sed ac urna finibus, bibendum diam a, lacinia leo. Quisque ac lacinia nunc, non egestas tellus. In cursus pellentesque velit, quis euismod erat rhoncus sed. Nulla in lorem condimentum, accumsan elit ac, lobortis felis. Aliquam consectetur tincidunt posuere. Pellentesque sodales iaculis diam, a imperdiet mi posuere vitae. Sed lacinia ex tincidunt blandit mollis.
+
+In fermentum posuere nulla sed euismod. Morbi faucibus vel metus vitae venenatis. Sed nisi arcu, placerat vel elit a, consequat hendrerit turpis. Vestibulum blandit imperdiet sem. Quisque tristique tellus eget pretium vulputate. Suspendisse elementum sodales malesuada. Praesent consectetur consequat auctor. Morbi vitae nulla porttitor, venenatis purus at, commodo nulla. Praesent at sagittis massa, quis ultricies tellus. Donec pharetra mi ut fermentum ultricies. Suspendisse sodales nec ligula id venenatis. Morbi egestas euismod cursus. Curabitur et metus iaculis, ullamcorper justo a, blandit felis. Curabitur nec eros nec eros suscipit dictum quis id dolor. Sed et eleifend tellus. Duis sagittis leo eu suscipit porttitor.
+
+</p>
+            </div>
+
+        </div>
+        
+    )
+}
+}
+
+export default WhoAmI
