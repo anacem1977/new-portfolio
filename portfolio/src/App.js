4af33e167e19b89416c934b189429bc29aecd5c3
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-30 10:35:37 -0600

    nav bar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import react, {Component} from "react"
 import animate__animated from "animate.css"
 import { Route } from "react-router-dom"
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {fab} from "@fortawesome/free-brands-svg-icons"
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons"
+
 import WhoAmI from "./components/whoAmI"
 import WhereIveBeen from "./components/whereIveBeen"
 import WhatIveDone from "./components/whatIveDone"
 import WhereImGoing from "./components/whereImGoing"
 import FunFacts from "./components/funFacts"
+
+library.add(fab, faEnvelope)
 
 class App extends Component {
   constructor(props) {
@@ -51,9 +58,19 @@ class App extends Component {
         <section className="animate__animated animate__fadeInDown animate__delay-1s">
             <p>Menu</p>
         </section>
+
+        <nav>
+          <p><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x"></FontAwesomeIcon></p>
+          <p><FontAwesomeIcon icon={['fab', 'facebook-messenger']} size="2x"></FontAwesomeIcon></p>
+          <p> <FontAwesomeIcon icon={['fab', 'github']} size="2x"></FontAwesomeIcon></p>
+          <p><FontAwesomeIcon icon="envelope" size="2x"></FontAwesomeIcon></p>
+        </nav>
+
         <div className="hola">
             {this.state.change === true ? <h1 onMouseEnter={this.handleEnglish} className="animate__animated animate__swing msg">HELLO</h1> : <h1 onMouseEnter={this.handleSpanish} className="animate__animated animate__bounce msg">HOLA</h1> }
+           
         </div>
+
         <div>
             <p className="scroll"> ← scroll</p>
         </div>
