4e3d0e4a6e94489ee72e863162621d8dae2e57b6
anace-m <maldonadoanaceciliac@johndeere.com>
2021-04-12 08:31:01 -0500

    laptop responsive

--- portfolio/src/components/whereIveBeen.js
+++ portfolio/src/components/whereIveBeen.js
@@ -7,7 +7,7 @@ class WhereIveBeen extends Component {
 render () {
     return (
         <div className="subtitle">
-            <div>
+            <div className="where">
                 <h3>Where I've been?</h3>
                 <p>I have a major in Industrial Engineering from Tecnológico de Monterrey and I've worked for John Deere for a little over nineteen years at the Engines (Motores) facility located in Torreón, Coahuila, México with twelve years of experience in Product Engineering and Specifications and about seven years in Supply Management for major diesel engine components.</p>
                 <p>During this time, I've also had the opportunity to participate in numerous projects such as SAP implementation, sunset of one of the engines that was produced at the factory, asset management and many more. →<Link to = "/resume" className="ga"> Read more</Link></p>
